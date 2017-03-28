0d24e73e9e846a6bfb086d802af339a24800a8bd
Filip Schouwenaars <filip@datacamp.com>
2017-03-28 10:50:17 +0200

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -31,7 +31,7 @@ rm $zipfile $tarfile
 
 ## Define versions (override shared image for easier development)
 SQLWHAT_VERSION="v1.1.1"
-SQLBACKEND_VERSION="v0.0.21"
+SQLBACKEND_VERSION="v0.0.23"
 # NOTE: using defaults again
 #pip3 install git+https://github.com/datacamp/sqlwhat@$SQLWHAT_VERSION
 #pip3 install git+https://$GITHUB_TOKEN@github.com/datacamp/sqlbackend.git/@$SQLBACKEND_VERSION
